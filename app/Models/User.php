ceb7289ec5296d99cd3fcad75f059c3afa4e7ba3
abdulrahmanFaid <abdulrahman_faid@outlook.com>
2019-10-03 19:39:41 +0200

    Change models directory

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -36,4 +36,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function channels()
+    {
+        return $this->hasMany(Channel::class);
+    }
 }
